0412db9ce9b4adb4b145d4961cb40addaaf4d861
anand krishna <ravarana@amazon.com>
2024-03-11 12:50:40 +0530

    add additional resource type and fixes

--- deployments/aft-account-customizations/LOGGING/terraform/main.tf
+++ deployments/aft-account-customizations/LOGGING/terraform/main.tf
@@ -18,8 +18,8 @@ module "manage_sink" {
   source                       = "./modules/sink"
   sink_name                    = "CentralLoggingSink"
   allowed_oam_resource_types   = ["AWS::Logs::LogGroup", "AWS::CloudWatch::Metric", "AWS::XRay::Trace", "AWS::ApplicationInsights::Application"]
-  allowed_source_accounts      = ["988814911723", "195274893729", "230253882749", "008732538448"]
-  allowed_source_organizations = ["o-pd4az3tlnp"]
+  allowed_source_accounts      = [""] # provide comma seperated values
+  allowed_source_organizations = [""] # provide comma seperated values
 }
 
 
